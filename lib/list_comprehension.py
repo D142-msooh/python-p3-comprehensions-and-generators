1b3ace57e3758f249b17fbf8491ee2521554b749
D142-msooh <kinyajuidama176@gmail.com>
2025-11-17 08:06:12 +0300

    Add list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
     pass
+#print(return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 def make_exclamation(sentence_list):
+    return [sentence + "!" for sentence in sentence_list]
     pass
+print(make_exclamation(["hello", "world", "this", "is", "great"]))
