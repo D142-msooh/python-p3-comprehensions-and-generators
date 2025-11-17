89142411755f0ac7f10695cbb720cbb388a2033d
D142-msooh <kinyajuidama176@gmail.com>
2025-11-17 20:26:47 +0300

    Add list_comrehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -3,7 +3,7 @@
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
     pass
-#print(return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(return_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 def make_exclamation(sentence_list):
     return [sentence + "!" for sentence in sentence_list]
